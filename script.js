99b29ff18ed95c2647b4d06c8118a3520814e19d
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-24 11:45:17 +0530

    brute force solution

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
-  //your code here
+  //brute force approach O(N^3)Time and O(1) Space
+  let closeSum = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        let currentSum = arr[i] + arr[j] + arr[k];
+
+        let currentDifference = Math.abs(target - currentSum);
+        let previousDifference = Math.abs(target - closeSum);
+        if (currentDifference < previousDifference) {
+          closeSum = currentSum;
+        }
+      }
+    }
+  }
+  return closeSum;
 }
 
 module.exports = threeSum;
